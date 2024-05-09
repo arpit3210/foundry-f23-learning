02f9afc0b59ac17a10e8125899b919493a4e41ca
Arpit Singh <arpitsinghthakur321@gmail.com>
2024-05-09 17:13:44 +0530

    first commit

---
+++ foundry-fund-me-23/test/FundMeTest.t.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
+
+contract FundMeTest is Test {
+
+FundMe fundMe;
+
+function setUp() external {
+
+DeployFundMe deployFundMe = new DeployFundMe();
+fundMe = deployFundMe.run();
+
+fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+}
+
+function testMinimumDollarIsFive() view public {
+assertEq(fundMe.MINIMUM_USD(), 5e18);
+}
+
+function testMsgSenderIsOwner() view public {
+//  assertEq(fundMe.i_owner(), msg.sender);
+assertEq(fundMe.i_owner(), address(this));
+assertNotEq(fundMe.i_owner(), msg.sender);
+console.log(fundMe.i_owner());
+console.log(address(this));
+console.log(msg.sender);
+}
+function testPriceFeedVersionIsAccurate() view public {
+    uint256 version = fundMe.getVersion();
+    assertEq(version, 4);
+    console.log(version);
+    // assertNotEq(version, 4);
+}
+
+}
